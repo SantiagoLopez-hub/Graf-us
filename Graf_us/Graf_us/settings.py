05027168a10ea8af83c8f37482165d58221869e1
SantiagoLopez-hub <46728986+SantiagoLopez-hub@users.noreply.github.com>
2021-11-19 14:07:47 +0000

    Creating views

--- Graf_us/Graf_us/settings.py
+++ Graf_us/Graf_us/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cqsrb=kyv%=i(0_85u013i!d8=nsunc@dwii5@is$g%3l6ry-p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://127.0.0.1']
 
 
 # Application definition
