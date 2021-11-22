5ca936a5aafc39a33235110fe825364820b988c1
SantiagoLopez-hub <46728986+SantiagoLopez-hub@users.noreply.github.com>
2021-11-22 11:46:23 +0000

    Configuring DB

--- Graf_us/Graf_us/settings.py
+++ Graf_us/Graf_us/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'Graf_us.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.postgresql',
+        # Port 5432 is most common for postgresql
     }
 }
 
