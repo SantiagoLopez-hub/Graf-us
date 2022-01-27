a2033610cd579bd8e00874aeed7093201d163e03
Fabian Simon <77444050+fabiansimon@users.noreply.github.com>
2022-01-27 20:03:15 +0000

    reponsiveness, first auth, conditional rendering

--- frontend/src/pages/AuthScreen.js
+++ frontend/src/pages/AuthScreen.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Center, useBreakpointValue } from '@chakra-ui/react';
 import FactText from '../components/FactText';
 import AuthView from '../components/AuthView';
 import { ReactComponent as LogoSVG } from '../assets/resources/tempLogo.svg';
 import { colors } from '../themeData';
 import CText from '../components/CText';
-import { Box, Center } from '@chakra-ui/react';
-
 
 function AuthScreen() {
+  const isMobile = useBreakpointValue({ base: true, lg: false })
+  const [isSignIn, setIsSignIn] = useState(true);
+
     return (
       <Box>
-        <FactText style={styles.leftFact} title={'Did you know?'} string={'93% of recruiters use LinkedIn to research and recruit candidates*'}/>
-        <CText style={styles.leftSource}>*According to statistics provided by linked on</CText>
+        { !isMobile && <FactText style={styles.leftFact} title={'Did you know?'} string={'93% of recruiters use LinkedIn to research and recruit candidates*'}/> }
+        { !isMobile && <CText style={styles.leftSource}>*According to statistics provided by linked on </CText>}
         <Box alignItems='center'>
           <Center>
             <LogoSVG style={{ marginBottom: 12 }}/>
           </Center>
-          <AuthView />  
+          <AuthView isSignIn={isSignIn} setIsSignIn={setIsSignIn}/>  
         </Box>
-        <FactText style={styles.rightFact} title={'Did you also know?'} string={'Using our advanced algorithms and revolutionary blockchain implementation you can expand your network like crazy**'}/>
-        <CText style={styles.rightSource}>**According to our completely biased opinion</CText>
+        { !isMobile && <FactText style={styles.rightFact} title={'Did you also know?'} string={'Using our advanced algorithms and revolutionary blockchain implementation you can expand your network like crazy**'}/> }
+        { !isMobile && <CText style={styles.rightSource}>**According to our completely biased opinion </CText>}
       </Box>
     );
   }
