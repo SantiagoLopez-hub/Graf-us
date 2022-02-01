170b2f66a5adf04c5ff0ade6c17d09e3ad56e662
SantiagoLopez-hub <silp4477@gmail.com>
2022-02-01 23:30:24 +0000

    Updating User model

--- Graf_us/api/models.py
+++ Graf_us/api/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class User(models.Model):
+    email = models.CharField(max_length=320, unique=True, blank=False, default=None)
+    password = models.CharField(max_length=256, blank=False, default=None)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField(max_length=3)
+    date_of_birth = models.DateTimeField()
+    gender = models.CharField(max_length=1)
     country = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     contact_number = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    email = models.CharField(max_length=8, default="", unique=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.IntegerField(max_length=3)
-    date_of_birth = models.DateTimeField(auto_now_add=True)
-    gender = models.CharField(max_length=1)
     created_at = models.DateTimeField(auto_now_add=True)
