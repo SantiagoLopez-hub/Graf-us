06d04a3ac6b97906280f2b210d23f27d53ea11bc
SantiagoLopez-hub <silp4477@gmail.com>
2022-02-05 00:17:29 +0000

    Merge Django with React

--- Graf_us/api/models.py
+++ Graf_us/api/models.py
@@ -1,29 +1,23 @@
 from django.db import models
+import string
+import random
 
 
-# https://docs.djangoproject.com/en/4.0/topics/db/examples/
-class User(models.Model):
-    email = models.EmailField(unique=True, blank=False, default=None)
-    password = models.CharField(max_length=256, blank=False, default=None)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.IntegerField(max_length=3)
-    date_of_birth = models.DateTimeField()
-    gender = models.CharField(max_length=1)
-    country = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    contact_number = models.CharField(max_length=20)
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
+
+
+# Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-class Connection(models.Model):
-    # Many to one Relationship
-    # from_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # to_user = models.ForeignKey(User)
-
-    tr_id
-    req_res_details
-    approval_status
-    f_email
-    email_id
--- Graf_us/frontend/src/App.js
+++ Graf_us/frontend/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { colors } from './themeData';
 import AuthScreen from './pages/AuthScreen';
 import { Box } from '@chakra-ui/react';
+import GrafScreen from './pages/GrafScreen';
 
 
 
 function App() {
   return (
     <Box style={styles.container}>
-      <AuthScreen />
+      {/* <AuthScreen /> */}
+      <GrafScreen />
     </Box>
   );
 }
@@ -17,7 +19,6 @@ const styles = {
   container: {
     display: 'flex', 
     justifyContent:'center', 
-    alignItems:'center', 
     width: '100%',
     height: '100vh',
     backgroundColor: colors.backgroundColor,
