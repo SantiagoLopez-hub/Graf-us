39a73d8d3f33ec2ab63aab3404a8872754e2504f
SantiagoLopez-hub <silp4477@gmail.com>
2022-02-06 21:43:34 +0000

    Reset postgresql settings

--- Graf_us/Graf_us/settings.py
+++ Graf_us/Graf_us/settings.py
@@ -87,9 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'PASSWORD':'grafus',
-        'USER': 'postgres'
+        'PORT': '5432'
     }
 }
 
