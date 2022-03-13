a49a24889cc69b9e7a1b2956bdcdd966349244e8
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-13 13:44:41 +0000

    Enable serialisers

--- Graf_us/api/urls.py
+++ Graf_us/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UserView#, ConnectionView, ProfileView, PostView
+from .views import UserView, ConnectionView, ProfileView, PostView
 
 urlpatterns = [
     path('user', UserView.as_view()),
-    # path('connection', ConnectionView.as_view()),
-    # path('profile', ProfileView.as_view()),
-    # path('post', PostView.as_view())
+    path('connection', ConnectionView.as_view()),
+    path('profile', ProfileView.as_view()),
+    path('post', PostView.as_view())
 ]
--- Graf_us/api/views.py
+++ Graf_us/api/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import User#, Connection, Profile, Post
-from .serializers import UserSerializer#, ConnectionSerializer, ProfileSerializer, PostSerializer
+from .models import User, Connection, Profile, Post
+from .serializers import UserSerializer, ConnectionSerializer, ProfileSerializer, PostSerializer
 
 
 class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-#
-#
-# class ConnectionView(generics.CreateAPIView):
-#     queryset = Connection.objects.all()
-#     serializer_class = ConnectionSerializer
-#
-#
-# class ProfileView(generics.CreateAPIView):
-#     queryset = Profile.objects.all()
-#     serializer_class = ProfileSerializer
-#
-#
-# class PostView(generics.CreateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-#
+
+
+class ConnectionView(generics.CreateAPIView):
+    queryset = Connection.objects.all()
+    serializer_class = ConnectionSerializer
+
+
+class ProfileView(generics.CreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class PostView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
