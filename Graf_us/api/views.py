dcb0a1d89e50a748ee237d67a789753a36aa46ed
Santiago <silp4477@gmail.com>
2022-03-13 09:25:29 +0000

    Rearranging database table creation order

--- Graf_us/api/views.py
+++ Graf_us/api/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import User, Connection, Profile
-from .serializers import UserSerializer, ConnectionSerializer, ProfileSerializer
+from .models import User#, Connection, Profile, Post
+from .serializers import UserSerializer#, ConnectionSerializer, ProfileSerializer, PostSerializer
 
 
 class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class ConnectionView(generics.CreateAPIView):
-    queryset = Connection.objects.all()
-    serializer_class = ConnectionSerializer
-
-
-class ProfileView(generics.CreateAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
+#
+#
+# class ConnectionView(generics.CreateAPIView):
+#     queryset = Connection.objects.all()
+#     serializer_class = ConnectionSerializer
+#
+#
+# class ProfileView(generics.CreateAPIView):
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
+#
+#
+# class PostView(generics.CreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
