ab41207e1f57b4fb4cde0d8e5da4c2c30e1bb9a0
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-23 22:57:14 +0000

    Cleaning code

--- Graf_us/api/tests.py
+++ Graf_us/api/tests.py
@@ -16,10 +16,12 @@ class UserTestCase(TestCase):
     def setUp(self):
         User.objects.create(
             email=self.email1,
-            password=self.password1)
+            password=self.password1
+        )
         User.objects.create(
             email=self.email2,
-            password=self.password2)
+            password=self.password2
+        )
 
     def test_users_can_create_profile(self):
         user1 = User.objects.get(email=self.email1)
@@ -40,7 +42,8 @@ class UserTestCase(TestCase):
         user2 = User.objects.get(email=self.email2)
         connection = Connection.objects.create(
             from_user=user1,
-            to_user=user2)
+            to_user=user2
+        )
 
         self.assertEqual(user1, connection.from_user)
         self.assertEqual(user2, connection.to_user)
