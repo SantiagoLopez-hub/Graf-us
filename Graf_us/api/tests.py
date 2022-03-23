4aff7b7bfe58593c8cbe28e8497160e65b3e0834
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-23 21:44:53 +0000

    Create testing use case class

--- Graf_us/api/tests.py
+++ Graf_us/api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from api.models import User
 
-# Create your tests here.
+
+# Test users
+class UserTestCase(TestCase):
+    email1 = 'a'
+    email2 = 'b'
+    password1 = ''
+    password2 = ''
+
+    def setUp(self):
+        User.objects.create(
+            email=self.email1,
+            password=self.password1)
+        User.objects.create(
+            email=self.email2,
+            password=self.password2)
