3754a033272a211cd2ec5fded6475d6f068baf75
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-29 16:36:10 +0100

    Attempt linkedin save

--- Graf_us/api/views.py
+++ Graf_us/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import User, Connection, Profile, Post
 from .serializers import UserSerializer, ConnectionSerializer, ProfileSerializer, PostSerializer
 
@@ -23,3 +25,42 @@ class PostView(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+
+class CreateUserView(APIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        ser = self.serializer_class(data=request.data)
+
+        if ser.is_valid():
+            email = ser.data.get('email')
+            password = ser.data.get('password')
+            first_name = ser.data.get('first_name')
+            last_name = ser.data.get('last_name')
+            date_of_birth = ser.data.get('date_of_birth')
+            created_at = ser.data.get('created_at')
+            host = self.request.session.session_key
+            qs = User.objects.filter(host=host)
+
+            if qs.exists():
+                user = qs[0]
+                user.email = email
+                user.password = password
+                user.first_name = first_name
+                user.last_name = last_name
+                user.date_of_birth = date_of_birth
+                user.created_at = created_at
+                user.save(update_fields=['email', 'password', 'first_name', 'last_name', 'date_of_birth', 'created_at'])
+            else:
+                user = User(host=host,
+                            email=email,
+                            password=password,
+                            first_name=first_name,
+                            last_name=last_name,
+                            date_of_birth=date_of_birth,
+                            created_at=created_at)
+                user.save()
+
+            return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
--- Graf_us/frontend/src/components/AuthView.js
+++ Graf_us/frontend/src/components/AuthView.js
@@ -8,62 +8,77 @@ import LinkedInButton from './LinkedInButton';
 
 
 function AuthView(props) {
-    const { linkedInLogin } = useLinkedIn({
-        clientId: '78y1jk156vmhwy',
-        redirectUri: window.location.origin + '/auth/linkedin/callback',
-        onSuccess: (code) => {
-          //  create user in database
-        },
-        onError: (error) => {
-          console.log(error);
-        },
-      });
+	let [token] = '';
+	const { linkedInLogin } = useLinkedIn({
+		clientId: '78y1jk156vmhwy',
+		redirectUri: window.location.origin + '/auth/linkedin/callback',
+		onSuccess: (code) => {
+			//  create user in database
+			fetch(window.location.origin + '/api/saveUser', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(code),
+			})
+				.then(response => response.json())
+				.then(data => {
+					token = data
+				})
+				.catch((error) => {
+					console.error('Error:', error);
+				});
+		},
+		onError: (error) => {
+			console.log(error);
+		},
+	});
 
-      console.log(props.isSignIn);
+	console.log(props.isSignIn);
 
-      return (
-        <Box>
-            <Box style={{...styles.container, ...props.style}}>
-                <Box>
-                    <CText textStyle={textStyles.headline3} style={{ paddingBottom: 15 }}>Hey there!</CText>
-                    <CText textStyle={textStyles.body1} style={{ color: colors.darkGrey, width: 230, marginBottom: 'auto', lineHeight: 1.40 }}>{props.isSignIn ? 'Sign ' : 'Login ' }in with you LinkedIn so we can get started to help you expand your network 🌎</CText>
-                </Box>
-                <LinkedInButton onClick={linkedInLogin} label={`${props.isSignIn ? 'Sign in ' : 'Log in '} in with LinkedIn`}/>
-            </Box>
-            <Box style={styles.loginText}>
-                <CText textStyle={textStyles.subtitle2} style={{ color: colors.grey, paddingRight: 6 }}>{props.isSignIn ? 'Already have an account?' : 'Not signed up yet?'}</CText>
-                <Link color={colors.blue} href='#' onClick={() => props.setIsSignIn(!props.isSignIn)}>
-                    <CText textStyle={textStyles.subtitle2} style={{ color: colors.blue.main }}>{props.isSignIn ? 'Login ' : 'Sign up ' }instead </CText>
-                </Link>
-            </Box>
-        </Box>
-    );
+	return (
+		<Box>
+			<Box style={{...styles.container, ...props.style}}>
+				<Box>
+					<CText textStyle={textStyles.headline3} style={{ paddingBottom: 15 }}>Hey there!</CText>
+					<CText textStyle={textStyles.body1} style={{ color: colors.darkGrey, width: 230, marginBottom: 'auto', lineHeight: 1.40 }}>{props.isSignIn ? 'Sign ' : 'Login ' }in with you LinkedIn so we can get started to help you expand your network 🌎</CText>
+				</Box>
+				<LinkedInButton onClick={linkedInLogin} label={`${props.isSignIn ? 'Sign in ' : 'Log in '} in with LinkedIn`}/>
+			</Box>
+			<Box style={styles.loginText}>
+				<CText textStyle={textStyles.subtitle2} style={{ color: colors.grey, paddingRight: 6 }}>{props.isSignIn ? 'Already have an account?' : 'Not signed up yet?'}</CText>
+				<Link color={colors.blue} href='#' onClick={() => props.setIsSignIn(!props.isSignIn)}>
+					<CText textStyle={textStyles.subtitle2} style={{ color: colors.blue.main }}>{props.isSignIn ? 'Login ' : 'Sign up ' }instead </CText>
+				</Link>
+			</Box>
+		</Box>
+	);
 }
 
 const styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        paddingLeft: 25,
-        paddingRight: 25,
-        paddingTop: 25,
-        paddingBottom: 25,
-        width: 285,
-        backgroundColor: colors.white,
-        borderRadius: 20, 
-        boxShadow: shadows.xl.main
-    },
-    loginText: {
-        flexDirection: 'row', 
-        display: 'flex', 
-        justifyContent: 'center', 
-        paddingTop: 10,
-    },
-    logo: {
-        marginLeft: 150,
-        marginBottom: 15,
-    }
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		paddingLeft: 25,
+		paddingRight: 25,
+		paddingTop: 25,
+		paddingBottom: 25,
+		width: 285,
+		backgroundColor: colors.white,
+		borderRadius: 20,
+		boxShadow: shadows.xl.main
+	},
+	loginText: {
+		flexDirection: 'row',
+		display: 'flex',
+		justifyContent: 'center',
+		paddingTop: 10,
+	},
+	logo: {
+		marginLeft: 150,
+		marginBottom: 15,
+	}
 }
 
 export default AuthView;
